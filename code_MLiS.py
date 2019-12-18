a239a6f5dc5804034cd1bf5a0e96dd4c100d1889
Pooja N Babu <poojanbabu24@gmail.com>
2019-12-18 18:54:57 +0000

    - Add PCA component plots

--- code_MLiS.py
+++ code_MLiS.py
@@ -186,18 +186,18 @@ brcancer_sc.replace([np.inf, -np.inf], 0, inplace=True)
 nr.seed(9988)
 Features = np.array(brcancer_sc)
 indx = range(Features.shape[0])
-indx = ms.train_test_split(indx, test_size=171)
+indx = ms.train_test_split(indx, test_size=114)
 
-x_train = Features[indx[0],2:]
-x_test = Features[indx[1],2:]
+x_train = Features[indx[0],:]
+x_test = Features[indx[1],:]
 
 # Rescale numeric features
-scaler = preprocessing.StandardScaler().fit(x_train[:, 2:])
-x_train[:, 2:] = scaler.transform(x_train[:, 2:])
-x_test[:, 2:] = scaler.transform(x_test[:, 2:])
+scaler = preprocessing.StandardScaler().fit(x_train[:,:])
+x_train[:, :] = scaler.transform(x_train[:,:])
+x_test[:, :] = scaler.transform(x_test[:, :])
 
 print(x_train.shape)
-plot_density_hist(x_train,cols, bins=20, hist=True, name='scaled_dens_')
+#plot_density_hist(x_train,cols, bins=20, hist=True, name='scaled_dens_')
 
 pca_mod = skde.PCA()
 pca_comps = pca_mod.fit(x_train)
@@ -218,6 +218,121 @@ Comps = pca_mod_5.transform(x_train)
 Comps.shape
 Comps_test=pca_mod_5.transform(x_test)
 
+# number of components
+n_pcs = pca_mod_5.components_.shape[0]
+
+most_important = [5, 13, 6, 9, 19, 14]
+def most_important_features(comps):
+    features = []
+    for j in most_important:
+        features.append(abs(comps[j]))
+
+    return features
+
+pca_comp_values = {}
+initial_feature_names = cols
+for i in range(n_pcs):
+    comps = pca_mod_5.components_[i]
+    idx = 'PCA'+ str(i);
+    pca_comp_values[idx] = most_important_features(comps)
+
+# PCA plot
+# Setting the positions and width for the bars
+pos = list(range(len(pca_comp_values['PCA0'])))
+width = 0.1
+palette = sns.diverging_palette(220, 20, n = 8)
+
+# Plotting the bars
+fig, ax = plt.subplots(figsize=(10, 5))
+
+# Create a bar with pre_score data,
+# in position pos,
+plt.bar(pos,
+        # using df['pre_score'] data,
+        pca_comp_values['PCA0'],
+        # of width
+        width,
+        # with alpha 0.5
+        alpha=0.5,
+        # with color
+        color=palette[0],
+        # with label the first value in first_name
+        label=cols[most_important[0]])
+
+# Create a bar with mid_score data,
+# in position pos + some width buffer,
+plt.bar([p + width for p in pos],
+        # using df['mid_score'] data,
+        pca_comp_values['PCA1'],
+        # of width
+        width,
+        # with alpha 0.5
+        alpha=0.5,
+        # with color
+        color=palette[2],
+        # with label the second value in first_name
+        label=cols[most_important[1]])
+
+# Create a bar with post_score data,
+# in position pos + some width buffer,
+plt.bar([p + width * 2 for p in pos],
+        # using df['post_score'] data,
+        pca_comp_values['PCA2'],
+        # of width
+        width,
+        # with alpha 0.5
+        alpha=0.5,
+        # with color
+        color=palette[5],
+        # with label the third value in first_name
+        label=cols[most_important[2]])
+
+plt.bar([p + width * 3 for p in pos],
+        # using df['post_score'] data,
+        pca_comp_values['PCA3'],
+        # of width
+        width,
+        # with alpha 0.5
+        alpha=0.5,
+        # with color
+        color=palette[6],
+        # with label the third value in first_name
+        label=cols[most_important[3]])
+
+plt.bar([p + width * 4 for p in pos],
+        # using df['post_score'] data,
+        pca_comp_values['PCA4'],
+        # of width
+        width,
+        # with alpha 0.5
+        alpha=0.5,
+        # with color
+        color=palette[7],
+        # with label the third value in first_name
+        label=cols[most_important[4]])
+
+# Set the y axis label
+ax.set_ylabel('Importance Score')
+
+# Set the chart's title
+ax.set_title('Features')
+
+# Set the position of the x ticks
+ax.set_xticks([p + 1.5 * width for p in pos])
+
+# Set the labels for the x ticks
+ax.set_xticklabels(cols[most_important[i]] for i in range(0, len(most_important)))
+
+# Setting the x-axis and y-axis limits
+plt.xlim(min(pos) - width, max(pos) + width * 4)
+plt.ylim([0, max(pca_comp_values['PCA0'] + pca_comp_values['PCA1'] + pca_comp_values['PCA2'] + pca_comp_values['PCA3'] + pca_comp_values['PCA4'])])
+
+# Adding the legend and showing the plot
+plt.legend(['PCA0', 'PCA1', 'PCA2', 'PCA3', 'PCA4'], loc='upper left')
+plt.grid()
+plt.show()
+
+
 km_models = []
 assignments_km = []
 assignments_test_km = []
@@ -231,9 +346,9 @@ for i in range(2,5):
     assignments_km.append(assignments_km2)
     assignments_test_km.append(test_values)
 
-def create_labels(assign,brcancer_indx,num_labels):
+def create_labels(assign,brcancer_indx):
      labels = np.zeros_like(assign)
-     for i in range(num_labels):
+     for i in range(2):
          mask = (assign == i)
          digits_mask = brcancer_indx[mask]
          labels[mask] = mode(digits_mask)[0]
@@ -252,6 +367,7 @@ ax1.set_title('k-means clustering plot')
 
 ax2.scatter(Comps[:,0],Comps[:,1],c = brcancer['Diagnosis'][indx[0]], cmap = "jet", edgecolor = "None", alpha=0.35)
 ax2.set_title('Actual clusters')
+plt.show()
 
 f, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
 
@@ -261,7 +377,7 @@ ax1.set_title('k-means clustering plot')
 ax2.scatter(Comps_test[:,0],Comps_test[:,1],c = brcancer['Diagnosis'][indx[1]], cmap = "jet", edgecolor = "None", alpha=0.35)
 ax2.set_title('Actual clusters')
 
-
+plt.show()
 def plot_clusters(sample, assignment):
     col_dic = {0:'blue',1:'red'}
     colors = [col_dic[x] for x in assignment]
@@ -329,11 +445,13 @@ VHC=np.zeros(shape=(10,nk))
 FMS=np.zeros(shape=(10,nk))
 CHS=np.zeros(shape=(10,nk))
 SIL=np.zeros(shape=(10,nk))
+
+pca_features = []
 for k in range(len(lims)-1):
     n = len(lims) - 1
     Features = np.array(brcancer_sc)
-    x_train = Features[lims[0]:lims[n],2:]
-    x_test = Features[lims[n-u]:lims[n-u+1],2:]
+    x_train = Features[lims[0]:lims[n],:]
+    x_test = Features[lims[n-u]:lims[n-u+1],:]
     train_set = np.arange(lims[0], lims[n], 1)
     test_set=np.arange(lims[n-u],lims[n-u+1],1)
     train_set = np.delete(train_set, test_set, 0)
@@ -367,4 +485,44 @@ for k in range(len(lims)-1):
         VHC[k,i-2]=metrics.v_measure_score(labels_true, labels_pred)
         FMS[k,i-2]=metrics.fowlkes_mallows_score(labels_true, labels_pred)
         CHS[k,i-2]=metrics.calinski_harabasz_score(Comps, labels_pred)
-        SIL[k,i-2]=metrics.silhouette_score(Comps,labels_pred)
+        SIL[k,i-2]=metrics.silhouette_score(Comps[:,0:1],labels_pred)
+
+performance_scores = {}
+
+def calculate_mean(scores):
+    means = []
+    for i in range(0,5):
+        mean = np.mean(scores[:, i])
+        means.append(mean)
+    return means
+
+
+performance_scores['ARI'] = calculate_mean(ARI)
+performance_scores['AMI'] = calculate_mean(AMI)
+performance_scores['NMI'] = calculate_mean(NMI)
+performance_scores['H'] = calculate_mean(H)
+performance_scores['C'] = calculate_mean(C)
+performance_scores['VHC'] = calculate_mean(VHC)
+performance_scores['FMS'] = calculate_mean(FMS)
+performance_scores['CHS'] = calculate_mean(CHS)
+performance_scores['SIL'] = calculate_mean(SIL)
+
+df=pd.DataFrame({'x': k_values, 'SIL': performance_scores['SIL'],
+                 'FMS': performance_scores['FMS'],
+                 'ARI': performance_scores['ARI'],
+                 'NMI': performance_scores['NMI']})
+plt.style.use('seaborn-darkgrid')
+palette = plt.get_cmap('Set1')
+
+num=0
+for column in df.drop('x', axis=1):
+    num+=1
+    plt.plot(df['x'], df[column], marker='', color=palette(num), linewidth=1, alpha=0.9, label=column)
+plt.legend(loc = 1)
+plt.title("Average Performance scores after cross validation", loc='left', fontsize=12, fontweight=0, color='black')
+plt.xlabel("Number of clusters")
+plt.ylabel("Performance Score")
+plt.show()
+
+# plt.plot(k_values, performance_scores['SIL'], 'r', k_values, performance_scores['FMS'], 'b', k_values, performance_scores['ARI'], 'g')
+# plt.show()
